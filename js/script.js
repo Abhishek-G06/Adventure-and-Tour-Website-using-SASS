ceb85e66bac1932850dc1e8a5ef41d67e3562e3e
Abhishek Gupta <abhi2002gupta@gmail.com>
2024-07-16 08:43:12 +0530

    shop script update

--- js/script.js
+++ js/script.js
@@ -38,6 +38,8 @@ window.onload = () => {
         document.querySelector('.header').classList.remove('.active');
     }
 }
+
+
 var swiper = new Swiper(".home-slider", {
     loop:true,
     grabCursor:true,
@@ -45,4 +47,28 @@ var swiper = new Swiper(".home-slider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     }
+});
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
 });
