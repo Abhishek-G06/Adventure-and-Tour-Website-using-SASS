4a862ec635653b42af91ee016d5b0668d45e8eeb
Abhishek Gupta <abhi2002gupta@gmail.com>
2024-07-16 13:02:15 +0530

    blog section slider

--- js/script.js
+++ js/script.js
@@ -25,24 +25,24 @@ window.onscroll = () => {
 
     if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
-    }else {
+    } else {
         document.querySelector('.header').classList.remove('active');
     }
 };
 
 
 window.onload = () => {
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('.active');
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('.active');
     }
 }
 
 
 var swiper = new Swiper(".home-slider", {
-    loop:true,
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -50,8 +50,8 @@ var swiper = new Swiper(".home-slider", {
 });
 
 var swiper = new Swiper(".product-slider", {
-    loop:true,
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     spaceBetween: 20,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -59,16 +59,58 @@ var swiper = new Swiper(".product-slider", {
     },
     breakpoints: {
         0: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         640: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
         1024: {
-          slidesPerView: 4,
+            slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
         },
     },
 });
